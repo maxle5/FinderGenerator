34a967a0cf7a2ea88d4cf4940aa2d521e5e4f00b
Max Lefebvre <max.lefebvre@garvin-allen.com>
2022-04-16 12:40:05 -0300

    initial commit

---
+++ src/Maxle5.Finder/FinderGenerator.cs
@@ -0,0 +1,309 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Text;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+
+namespace Maxle5.Finder
+{
+    [Generator]
+    public class FinderSourceGenerator : ISourceGenerator
+    {
+        private readonly Queue<char> _variableNames = new Queue<char>(new[]
+        {
+            'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','v','w','x','y','z'
+        });
+
+        public void Initialize(GeneratorInitializationContext context)
+        {
+#if DEBUG
+            // If you want to debug the Source Generator, please uncomment the below code.
+            if (!Debugger.IsAttached)
+            {
+                Debugger.Launch();
+            }
+#endif            
+
+            // Register the attribute source
+            context.RegisterForPostInitialization((i) => i.AddSource("FinderGeneratorAttribute.g.cs", FinderTemplates.Attribute));
+
+            // Register a syntax receiver that will be created for each generation pass
+            context.RegisterForSyntaxNotifications(() => new FinderSyntaxReceiver());
+        }
+
+        public void Execute(GeneratorExecutionContext context)
+        {
+            // retrieve the populated receiver 
+            if (context.SyntaxContextReceiver is not FinderSyntaxReceiver receiver)
+            {
+                return;
+            }
+
+            foreach (var finderMethodToGenerate in receiver.FinderMethodsToGenerate)
+            {
+                if (finderMethodToGenerate.Parameters.Length != 1)
+                {
+                    continue; // doesn't have a single parameter (this is a requirement)
+                }
+
+                if (!TryGetGenericArgumentTypeFromIEnumerable(finderMethodToGenerate.ReturnType as INamedTypeSymbol, out var typeToFind))
+                {
+                    continue; // doesn't return IEnumerable (this is a requirement)
+                }
+
+                var finderNamespace = finderMethodToGenerate.ContainingNamespace.ToString();
+                var finderClass = BuildClassWrapper(finderMethodToGenerate);
+                var finderMethod = BuildMethodWrapper(finderMethodToGenerate, typeToFind);
+                var parameter = finderMethodToGenerate.Parameters[0];
+                var typeToLookThrough = parameter.Type as INamedTypeSymbol;
+
+                var sourceCode = $@"namespace {finderNamespace}
+{{
+    {finderClass}
+    {{
+        {GenerateFinderClass(finderMethod, parameter.Name, typeToLookThrough, typeToFind)}
+    }}
+}}";
+
+                context.AddSource($"{finderMethodToGenerate.ContainingType.Name}.g.cs", SourceText.From(sourceCode, Encoding.UTF8));
+            }
+        }
+
+        private string GenerateFinderClass(
+            string methodSignature,
+            string parameterName,
+            INamedTypeSymbol typeToLookThrough,
+            INamedTypeSymbol typeToFind)
+        {
+            var sourceCodeBody = new StringBuilder();
+
+            GenerateFinderMethod(
+                sourceCodeBody,
+                parameterName,
+                typeToLookThrough,
+                typeToFind);
+
+            return $@"{methodSignature}
+        {{
+            var instances = new List<{typeToFind.ToDisplayString()}>();
+            {sourceCodeBody}
+            return instances;
+        }}";
+        }
+
+        private void GenerateFinderMethod(
+            StringBuilder sourceCode,
+            string currentPath,
+            INamedTypeSymbol currentType,
+            INamedTypeSymbol typeToFind)
+        {
+            // Check if the current type is an IEnumerable
+            if (TryGetGenericArgumentTypeFromIEnumerable(currentType, out var genericType))
+            {
+                // Continue traversing properties of T
+                var tempSourceCode = new StringBuilder();
+                var variableName = _variableNames.Dequeue();
+
+                if (genericType != null)
+                {
+                    GenerateFinderMethod(
+                        tempSourceCode,
+                        variableName.ToString(),
+                        genericType,
+                        typeToFind);
+
+                    // Check if there were any matches in type T
+                    if (tempSourceCode.Length > 0)
+                    {
+                        tempSourceCode.Insert(0, $"\nforeach(var {variableName} in {currentPath})\n{{\n");
+                        tempSourceCode.Append("}\n");
+                        sourceCode.Append(tempSourceCode.ToString()).Append('\n');
+                    }
+                }
+
+                // Variables are out of scope now, so we can add them to be used again
+                _variableNames.Enqueue(variableName);
+            }
+            else
+            {
+                if (currentType.Equals(typeToFind, SymbolEqualityComparer.Default) || currentType.Equals(typeToFind, SymbolEqualityComparer.Default))
+                {
+                    sourceCode.Append("instances.Add(").Append(currentPath).AppendLine(");\n");
+                }
+
+                if (TypeHasPropertiesToSearch(currentType))
+                {
+                    foreach (var property in currentType.GetMembers().OfType<IPropertySymbol>())
+                    {
+                        if (property.Type is INamedTypeSymbol propertyType)
+                        {
+                            GenerateFinderMethod(
+                                sourceCode,
+                                string.Concat(currentPath, ".", property.Name),
+                                propertyType,
+                                typeToFind);
+                        }
+                    }
+                }
+            }
+        }
+
+        private static bool TypeHasPropertiesToSearch(INamedTypeSymbol t)
+        {
+            switch (t.Name)
+            {
+                case "String":
+                case "char":
+                case "byte":
+                case "sbyte":
+                case "ushort":
+                case "short":
+                case "uint":
+                case "int":
+                case "ulong":
+                case "long":
+                case "float":
+                case "double":
+                case "decimal":
+                case "DateTime":
+                    return false;
+                default:
+                    return true;
+            }
+        }
+
+        private static string BuildClassWrapper(IMethodSymbol method)
+        {
+            var sb = new StringBuilder();
+
+            var @class = method.ContainingType;
+
+
+            var accessibility = @class.DeclaredAccessibility switch
+            {
+                Accessibility.Public => "public",
+                Accessibility.Private => "private",
+                Accessibility.Internal => "internal",
+                _ => string.Empty
+            };
+
+            sb.Append(accessibility);
+
+            if (@class.IsStatic)
+            {
+                sb.Append(" static");
+            }
+
+            if (method.IsPartialDefinition)
+            {
+                sb.Append(" partial");
+            }
+
+            if (@class.IsSealed)
+            {
+                sb.Append(" sealed");
+            }
+
+            if (@class.IsAbstract)
+            {
+                sb.Append(" abstract");
+            }
+
+            sb.Append(" class ");
+
+            sb.Append(@class.Name);
+
+            return sb.ToString();
+        }
+
+        private static string BuildMethodWrapper(IMethodSymbol method, INamedTypeSymbol typeToFind)
+        {
+            var sb = new StringBuilder();
+
+            var accessibility = method.DeclaredAccessibility switch
+            {
+                Accessibility.Public => "public",
+                Accessibility.Protected => "protected",
+                Accessibility.Private => "private",
+                Accessibility.Internal => "internal",
+                Accessibility.ProtectedAndInternal => "internal protected",
+                _ => string.Empty
+            };
+
+            sb.Append(accessibility);
+
+            if (method.IsStatic)
+            {
+                sb.Append(" static");
+            }
+
+            if (method.IsPartialDefinition)
+            {
+                sb.Append(" partial");
+            }
+
+            sb.Append(" IEnumerable<");
+            sb.Append(typeToFind.ToDisplayString());
+            sb.Append("> ");
+            sb.Append(method.Name);
+            sb.Append("(");
+            sb.Append(method.Parameters[0].Type.ToDisplayString());
+            sb.Append(" ");
+            sb.Append(method.Parameters[0].Name);
+            sb.Append(")");
+
+            return sb.ToString();
+        }
+
+        private static bool TryGetGenericArgumentTypeFromIEnumerable(
+            INamedTypeSymbol type,
+            out INamedTypeSymbol genericArgumentType)
+        {
+            genericArgumentType = null;
+            if (type.ConstructedFrom.Name == "IEnumerable")
+            {
+                var genericArgs = type.TypeArguments;
+                if (genericArgs.Length > 0)
+                {
+                    genericArgumentType = genericArgs[0] as INamedTypeSymbol;
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+
+    /// <summary>
+    /// Created on demand before each generation pass
+    /// </summary>
+    internal class FinderSyntaxReceiver : ISyntaxContextReceiver
+    {
+        /// <summary>
+        /// List of "Finder methods" to generate
+        /// </summary>
+        public List<IMethodSymbol> FinderMethodsToGenerate { get; } = new List<IMethodSymbol>();
+
+        /// <summary>
+        /// Called for every syntax node in the compilation, we can inspect the nodes and save any information useful for generation
+        /// </summary>
+        public void OnVisitSyntaxNode(GeneratorSyntaxContext context)
+        {
+            if (context.Node is MethodDeclarationSyntax methodDeclarationSyntax             // any method
+                && methodDeclarationSyntax.AttributeLists.Count > 0                         // with atleast 1 attribute
+                && methodDeclarationSyntax.Modifiers.Any(m => m.ValueText == "partial")     // that is partial
+                && methodDeclarationSyntax.ParameterList.Parameters.Count == 1)             // with 1 parameter
+            {
+                var methodSymbol = context.SemanticModel.GetDeclaredSymbol(methodDeclarationSyntax) as IMethodSymbol;
+
+                // Get the symbol being declared by the field, and keep it if its annotated
+                if (methodSymbol?.GetAttributes().Any(attr => attr.AttributeClass.ToDisplayString() == "Maxle5.Finder.FinderGeneratorAttribute") == true)
+                {
+                    FinderMethodsToGenerate.Add(methodSymbol);
+                }
+            }
+        }
+    }
+}
---
+++ tests/Maxle5.FinderPlayground/Test.cs
@@ -0,0 +1,78 @@
+﻿//using Maxle5.TypeFinder;
+
+//namespace Maxle5.TypeFinderPlayground
+//{    
+//    [FinderGenerator]
+//    public static class Test
+//    {
+//        public static void Main()
+//        {
+//            Maxle5.TypeFinder.DateTimeFinderHelper.DateTimeFinder(new Parent());
+//        }
+//    }
+//}
+
+namespace Maxle5.FinderPlayground
+{
+    public static class Program
+    {
+        public static void Main()
+        {
+            var ints = DateTimeFinder.Find(new Parent
+            {
+                Id = 1,
+                Name = "Alain",
+                Date = new DateTime(1965, 12, 07),
+                Children = new[]
+                {
+                    new Child
+                    {
+                        Id = 1,
+                        Name = "Max",
+                        Date = new DateTime(1995, 12, 30)
+                    },
+                    new Child
+                    {
+                        Id = 2,
+                        Name = "Luc",
+                        Date = new DateTime(1997, 12, 10)
+                    },
+                    new Child
+                    {
+                        Id = 3,
+                        Name = "Liam",
+                        Date = new DateTime(2000, 2, 17)
+                    }
+                }
+            });
+
+            foreach (var integer in ints)
+            {
+                Console.WriteLine(integer.ToString());
+            }
+        }
+    }
+
+    public class Parent
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public DateTime Date { get; set; }
+        public IEnumerable<Child> Children { get; set; }
+    }
+
+    public class Child
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public DateTime Date { get; set; }
+    }
+
+    public static partial class DateTimeFinder
+    {
+        [Finder.FinderGenerator]
+        public static partial IEnumerable<string> Find(Parent parent);
+    }
+}
