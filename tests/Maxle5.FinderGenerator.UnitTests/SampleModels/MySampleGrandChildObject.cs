3b274194eda256cf3a64c339e6efc8f8e009b62b
Max Lefebvre <max.lefebvre@garvin-allen.com>
2023-01-27 12:47:46 -0400

    fixed an issue where exception would be thrown when path contained null node

--- tests/Maxle5.FinderGenerator.UnitTests/SampleModels/MySampleGrandChildObject.cs
+++ tests/Maxle5.FinderGenerator.UnitTests/SampleModels/MySampleGrandChildObject.cs
@@ -1,11 +1,10 @@
 ﻿namespace Maxle5.FinderGenerator.UnitTests
 {
-    public class MySampleChildObject
+    public class MySampleGrandChildObject
     {
         public int Id { get; set; }
         public string Name { get; set; }
         public IEnumerable<int> Numbers { get; set; }
-        public IEnumerable<MySampleGrandChildObject> GrandChildren { get; set; }
     }
 
     //public static partial class StringFinder
